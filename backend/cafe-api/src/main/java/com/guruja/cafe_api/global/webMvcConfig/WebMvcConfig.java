7843d63cbf6a93cdfe69f08c672014920e303db2
wonmin <dnjsals114@gmail.com>
2025-09-26 17:38:02 +0900

    commit

--- backend/cafe-api/src/main/java/com/guruja/cafe_api/global/webMvcConfig/WebMvcConfig.java
+++ backend/cafe-api/src/main/java/com/guruja/cafe_api/global/webMvcConfig/WebMvcConfig.java
@@ -9,15 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-<<<<<<< HEAD
-        registry.addMapping("/**")      // 모든 경로
-                .allowedOrigins("*")    // 모든 출처 허용
-                .allowedMethods("*")    // 모든 HTTP 메서드 허용 (GET, POST 등)
-                .allowedHeaders("*");   // 모든 헤더 허용
-    }
-}
 
-=======
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
@@ -25,4 +17,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowCredentials(true);
     }
 }
->>>>>>> c358eb20a5cb6beb06d19e52cb0398588b3d2975
+
--- backend/cafe-api/src/main/java/com/guruja/cafe_api/order/controller/OrderController.java
+++ backend/cafe-api/src/main/java/com/guruja/cafe_api/order/controller/OrderController.java
@@ -19,11 +19,5 @@ public class OrderController {
         return ResponseEntity.ok(orderService.createOrder(orderDto));
     }
 
-    // 수동으로 호출해서 14시 이전 주문을 배송중으로 바꾸는 API
-    @PutMapping("/update-to-shipping")
-    public ResponseEntity<Void> updateMorningOrdersToShipping() {
-        orderService.updateMorningOrdersToShipping();
-        return ResponseEntity.ok().build();
-    }
 }
 
