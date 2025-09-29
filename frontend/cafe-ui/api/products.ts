6668054567c08a59a9b5a6a8151724b03e9f4597
jungwook <mr4454@naver.com>
2025-09-29 09:34:16 +0900

    solve dto conflict

--- frontend/cafe-ui/api/products.ts
+++ frontend/cafe-ui/api/products.ts
@@ -2,12 +2,7 @@ import api from "@/lib/axios";
 import { ProductListRes } from "@/types";
 
 
-export const fetchProducts = async (): Promise<Product[]> => {
-  const res = await api.get('/products');
-  return res.data as Product[];
-}
-
-export const fetchProducts2 = async (): Promise<ProductListRes[]> => {
+export const fetchProducts = async (): Promise<ProductListRes[]> => {
   const res = await api.get<ProductListRes[]>('/products');
   return res.data;
 }
